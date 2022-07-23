4a2967772ca4c9d95e2aeb4c1b582bd3aace24f0
annaburlakova <burlakova.anastasia.92@gmail.com>
2022-07-23 02:00:19 -0400

    search page locators are added

--- src/test/java/com/expedia/pages/SearchResultPage.java
+++ src/test/java/com/expedia/pages/SearchResultPage.java
@@ -1,4 +1,27 @@
 package com.expedia.pages;
 
+import com.expedia.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 public class SearchResultPage {
+
+    public SearchResultPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//select[@data-test-id='sortDropdown']")
+    public WebElement sortByDropdown;
+
+    @FindBy(xpath = "//button[@class='uitk-card-link']//span")
+    public List<WebElement> flightOptions;
+
+    @FindBy(xpath ="//button[@name='showMoreButton']")
+    public WebElement showMoreButton;
+
 }
